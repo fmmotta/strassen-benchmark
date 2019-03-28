daeb6232212f96cfb90205d64ca7a10318850591
fernando <fernando.motta@wikki.com.br>
2019-03-28 16:53:18 -0300

    Strassen function complete, needs sh script and structure to measure time

--- strassen.c
+++ strassen.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #define sgemm sgemm_
-#define sgetrf sgetrf_
 
 
 void sgemm (char *transA, char *transB, 
@@ -9,8 +9,6 @@ void sgemm (char *transA, char *transB,
             float *alpha, float *A, int *ldA, float *B, int *ldB,
             float *beta, float *C, int *ldC ) ; 
 
-void sgetrf( int *m, int *n, float *A, int *ldA, int *IPIV, int *info) ;
-void strsv (char *UPLO, char *TRANS, char *DIAG, int *n, float *A, int *ldA, float *X, int *incX) ;
 
 
 void imp_matriz(int m, int n, int ldA, float *A)
@@ -54,7 +52,7 @@ float strassen( float A, float B, int n, int min_size)
             float B_21[sq_size];    
             float B_22[sq_size];    
 
-            for (i = 0; i < sq_size - 1; ++i) {
+            for (i = 0; i < sq_size ; ++i) {
                 A_11[i] = A[i+block_size*(i/block_size)];        
                 A_12[i] = A[i+block_size*((block_size + i)/block_size)];        
                 A_21[i] = A[i+block_size*((4*block_size + i)/block_size)];        
@@ -116,7 +114,7 @@ float strassen( float A, float B, int n, int min_size)
             P_7 = strassen(P_71, B_11, block_size, min_size);
 
 
-            for (i = 0; i < sq_size - 1; ++i) {
+            for (i = 0; i < sq_size ; ++i) {
                 C[i+block_size*(i/block_size)] = P_1[i] + P_2[i] - P_4[i] + P_6[i];
                 C[i+block_size*((block_size + i)/block_size)] = P_4[i] + P_5[i];
                 C[i+block_size*((4*block_size + i)/block_size)] = P_6[i] + P_7[i];
@@ -124,42 +122,34 @@ float strassen( float A, float B, int n, int min_size)
             }
 
             return C;
+        }
 
-main()
+main(int argc, char **argv)
 
 {
-int IPIV[10];
-int   m=8;
-int   n=8;
-int   ldA=8;
-float A[n*ldA];
-float L[n*ldA];
-float U[n*ldA];
-int   INFO;
-int i,j ;
 
+    int n = (1 << argv[1]); //Bit shift trick to find powers of 2
+    int min_size[argv[2]];
+    int sq_size = n*n;
+    float A[sq_size];
+    float B[sq_size];
+    clock_t start, end;
+    double cpu_time_used_strassen, cpu_time_used_blas;
 
-srand48(1) ;
+    srand48(1) ;
 
-for (i=0; i<m;i++)
-   for (j=0; j<n; j++)
-       A[i+j*ldA] = drand48();
+    for (i=0; i<sq_size;i++)
+        A[i] = drand48();
+        B[i] = drand48();
 
-imp_matriz(m,n,ldA,A) ;
+    start = clock();
+    strassen(&A, &B, n, min_size);
+    end = clock();
+    cpu_time_used_strassen = ((double)(end - start))/CLOCKS_PER_SEC;
 
-for (i=0; i<m;i++)
-   for (j=0; j<n; j++)
-       U[i+j*ldA] = A[i+j*ldA]; 
-
-for(i=0; i<ldA; i++) IPIV[i]= 0;
-
-for(i=0; i<m; i++) printf("IPIV[%d]=%d\n", i, IPIV[i]) ;
-sgetrf(&m,&n,U, &ldA,IPIV, &INFO) ;
-
-printf("INFO=%d\n", INFO) ;
-for(i=0; i<m; i++) printf("IPIV[%d]=%d\n", i, IPIV[i]) ;
-imp_matriz(m,n,ldA,U) ;
-
-
+    start = clock();
+    sgemm("N","N", &n, &n, &n, 1.0, A, &n, B, &n, 0, C, &n);
+    end = clock();
+    cpu_time_used_blas = ((double)(end - start))/CLOCKS_PER_SEC;
 
 }
