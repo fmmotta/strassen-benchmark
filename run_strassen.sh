83a94254088a332c980c479b8df4938516021e2c
fernando <fernando.motta@wikki.com.br>
2019-04-02 09:38:55 -0300

    Works to larger n. Values are still a bit weird

--- run_strassen.sh
+++ run_strassen.sh
@@ -4,7 +4,7 @@ counter=1
 rm time_results.csv
 touch time_results.csv
 echo "n, time_strassen, time_blas" >> time_results.csv    
-while [ $counter -le 279 ]
+while [ $counter -le 1000 ]
 do    
     ./a.out $counter 1 >> time_results.csv 
     counter=$(( $counter + 1 )) 
--- strassen.c
+++ strassen.c
@@ -49,63 +49,38 @@ void strassen( float *A, float *B, float *C, int n, int min_size)
             //printf("sf 1 \n");
             int block_size = n/2;
             int sq_size = block_size*block_size;
-/*
-            float A_11[sq_size];    
-            float A_12[sq_size];    
-            float A_21[sq_size];    
-            float A_22[sq_size];    
-            float B_11[sq_size];    
-            float B_12[sq_size];    
-            float B_21[sq_size];    
-            float B_22[sq_size];    
-
-            //printf("sf 2 \n");
-            for (i = 0; i < sq_size ; ++i) {
-                A_11[i] = A[i+block_size*(i/block_size)];        
-                A_12[i] = A[i+block_size*((2*sq_size + i)/block_size)];        
-                A_21[i] = A[i+block_size*((block_size + i)/block_size)];        
-                A_22[i] = A[i+block_size*((2*sq_size + i + block_size)/block_size)];    
-
-                B_11[i] = B[i+block_size*(i/block_size)];        
-                B_12[i] = B[i+block_size*((2*sq_size + i)/block_size)];        
-                B_21[i] = B[i+block_size*((block_size + i)/block_size)];        
-                B_22[i] = B[i+block_size*((2*sq_size + i + block_size)/block_size)];        
-            }
             
-            imp_matriz(n,n,n,A);
-            printf("\n\n");
-            imp_matriz(n,n,n,B);
-            printf("\n\n");
-            
-            n = block_size;
-
-            printf("A11 \n\n");
-            imp_matriz(n,n,n,A_11);
-            printf("\n\n");
-            printf("A12 \n\n");
-            imp_matriz(n,n,n,A_12);
-            printf("\n\n");
-            printf("A_21 \n\n");
-            imp_matriz(n,n,n,A_21);
-            printf("\n\n");
-            printf("A22 \n\n");
-            imp_matriz(n,n,n,A_22);
-            printf("\n\n");
-            printf("B11 \n\n");
-            imp_matriz(n,n,n,B_11);
-            printf("\n\n");
-            printf("B12 \n\n");
-            imp_matriz(n,n,n,B_12);
-            printf("\n\n");
-            printf("B21 \n\n");
-            imp_matriz(n,n,n,B_21);
-            printf("\n\n");
-            printf("B22 \n\n");
-            imp_matriz(n,n,n,B_22);
-            printf("\n\n");
-*/
             //printf("sf 3 \n");
-            float P_1[sq_size];
+            float * P_1 = (float *) malloc(sq_size * sizeof(float));
+            float * P_2 = (float *) malloc(sq_size * sizeof(float));
+            float * P_3 = (float *) malloc(sq_size * sizeof(float));
+            float * P_4 = (float *) malloc(sq_size * sizeof(float));
+            float * P_5 = (float *) malloc(sq_size * sizeof(float));
+            float * P_6 = (float *) malloc(sq_size * sizeof(float));
+            float * P_7 = (float *) malloc(sq_size * sizeof(float));
+
+            float * P_11 = (float *) malloc(sq_size * sizeof(float));
+            float * P_12 = (float *) malloc(sq_size * sizeof(float));
+
+            float * P_21 = (float *) malloc(sq_size * sizeof(float));
+            float * P_22 = (float *) malloc(sq_size * sizeof(float));
+
+            float * P_31 = (float *) malloc(sq_size * sizeof(float));
+            float * P_32 = (float *) malloc(sq_size * sizeof(float));
+
+            float * P_41 = (float *) malloc(sq_size * sizeof(float));
+            float * P_42 = (float *) malloc(sq_size * sizeof(float));
+
+            float * P_51 = (float *) malloc(sq_size * sizeof(float));
+            float * P_52 = (float *) malloc(sq_size * sizeof(float));
+
+            float * P_61 = (float *) malloc(sq_size * sizeof(float));
+            float * P_62 = (float *) malloc(sq_size * sizeof(float));
+
+            float * P_71 = (float *) malloc(sq_size * sizeof(float));
+            float * P_72 = (float *) malloc(sq_size * sizeof(float));
+
+/*            float P_1[sq_size];
             float P_2[sq_size];
             float P_3[sq_size];
             float P_4[sq_size];
@@ -133,7 +108,7 @@ void strassen( float *A, float *B, float *C, int n, int min_size)
 
             float P_71[sq_size]; // A_21 + A22
             float P_72[sq_size]; // B11
-
+*/
             for (i = 0; i < sq_size; ++i) {
                 P_11[i] = A[i+block_size*((2*sq_size + i)/block_size)] - A[i+block_size*((2*sq_size + i + block_size)/block_size)];
                 P_12[i] = B[i+block_size*((block_size + i)/block_size)] + B[i+block_size*((2*sq_size + i + block_size)/block_size)]; 
@@ -157,55 +132,6 @@ void strassen( float *A, float *B, float *C, int n, int min_size)
                 P_72[i] = B[i+block_size*(i/block_size)]; //B_11 
             }
 
-/*            //printf("sf 4 \n");
-            for (i = 0; i < sq_size ; ++i) {
-                P_11[i] = A_12[i] - A_22[i];        
-                P_12[i] = B_21[i] + B_22[i];        
-
-                P_21[i] = A_11[i] + A_22[i];        
-                P_22[i] = B_11[i] + B_22[i];        
-
-                P_31[i] = A_11[i] - A_21[i];        
-                P_32[i] = B_11[i] + B_12[i];        
-
-                P_41[i] = A_11[i] + A_12[i];        
-                P_52[i] = B_12[i] - B_22[i];        
-                P_62[i] = B_21[i] - B_11[i];        
-                P_71[i] = A_21[i] + A_22[i];        
-            }
-
-            n = block_size;
-            printf("A12 - A22\n\n");
-            imp_matriz(n,n,n,P_11);
-            printf("\n\n");
-            printf("B21 + B22\n\n");
-            imp_matriz(n,n,n,P_12);
-            printf("\n\n");
-            printf("A11 + A22\n\n");
-            imp_matriz(n,n,n,P_21);
-            printf("\n\n");
-            printf("B11 + B22\n\n");
-            imp_matriz(n,n,n,P_22);
-            printf("\n\n");
-            printf("A11 - A21\n\n");
-            imp_matriz(n,n,n,P_31);
-            printf("\n\n");
-            printf("B11 + B12\n\n");
-            imp_matriz(n,n,n,P_32);
-            printf("\n\n");
-            printf("A11 + A12\n\n");
-            imp_matriz(n,n,n,P_41);
-            printf("\n\n");
-            printf("B12 - B22\n\n");
-            imp_matriz(n,n,n,P_52);
-            printf("\n\n");
-            printf("B21 - B11\n\n");
-            imp_matriz(n,n,n,P_62);
-            printf("\n\n");
-            printf("A21 + A22\n\n");
-            imp_matriz(n,n,n,P_71);
-            printf("\n\n");
-*/
             //printf("sf 5 \n");
             strassen(P_11, P_12, P_1, block_size, min_size);
             strassen(P_21, P_22, P_2, block_size, min_size);
@@ -223,6 +149,28 @@ void strassen( float *A, float *B, float *C, int n, int min_size)
                 C[i+block_size*((block_size + i)/block_size)] = P_6[i] + P_7[i];
                 C[i+block_size*((2*sq_size + block_size + i)/block_size)] = P_2[i] - P_3[i] + P_5[i] - P_7[i];
             }
+
+            free(P_1);
+            free(P_2);
+            free(P_3);
+            free(P_4);
+            free(P_5);
+            free(P_6);
+            free(P_7);
+            free(P_11);
+            free(P_12);
+            free(P_21);
+            free(P_22);
+            free(P_31);
+            free(P_32);
+            free(P_41);
+            free(P_42);
+            free(P_51);
+            free(P_52);
+            free(P_61);
+            free(P_62);
+            free(P_71);
+            free(P_72);
 
             //printf("sf 7 \n");
             return;
@@ -248,7 +196,6 @@ int main(int argc, char **argv)
     float * C = (float *) malloc(sq_size * sizeof(float));
     float * D = (float *) malloc(sq_size * sizeof(float));
     
-    float * R = (float *) malloc(sq_size * sizeof(float));
 
 
     //srand48(1) ;
@@ -271,13 +218,15 @@ int main(int argc, char **argv)
     sgemm("N","N", &n, &n, &n, &alpha, A, &n, B, &n, &beta, D, &n);
     end = clock();
 
+    //If checking for residue
+/*    float * R = (float *) malloc(sq_size * sizeof(float));
     for (int i=0; i<sq_size;i++){
         R[i] = D[i] - C[i];
     }
-
+*/
     cpu_time_used_blas = ((double)(end - start))/CLOCKS_PER_SEC;
 
-    //printf(" %i, %g, %g \n", n, cpu_time_used_strassen, cpu_time_used_blas); //print for script
+    printf(" %i, %g, %g \n", n, cpu_time_used_strassen, cpu_time_used_blas); //print for script
     //printf("TIMES: \n Time_strassen: %g \n Time_blas: %g \n",cpu_time_used_strassen, cpu_time_used_blas);
 /*    printf("A:  \n\n");
     imp_matriz(n,n,n,A);
